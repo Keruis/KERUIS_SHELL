529ad0a1762b9b0a394124539cd01d1d23669f9d
Keruis <wuyutong0105@gmail.com>
2025-10-20 13:08:05 +0800

    02

--- core/memory/allocator.h
+++ core/memory/allocator.h
@@ -90,12 +90,6 @@ public:
     allocator(const allocator<Up_>&) { }
 };
 
-extern template class allocator<char>;
-extern template class allocator<wchar_t>;
-extern template class allocator<char8_t>;
-extern template class allocator<char16_t>;
-extern template class allocator<char32_t>;
-
 }
 
 #endif //ALLOCATOR_H
